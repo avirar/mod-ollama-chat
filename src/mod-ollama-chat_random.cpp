13aff99ba7fb392d08930f219de59b2b54d54e14
avirar <luke.thorpe@gmail.com>
2025-05-28 17:13:26 +1000

    Update mod-ollama-chat_random.cpp

--- src/mod-ollama-chat_random.cpp
+++ src/mod-ollama-chat_random.cpp
@@ -341,8 +341,8 @@ void OllamaBotRandomChatter::HandleRandomChatter()
                 "You are currently located in {}, inside the zone '{}' on map '{}'. "
                 "Your Personality is '{}'. "
                 "You glance around the world. Make an observation and make sure to reference the main subject item: {}. "
-                "Comment aloud in character with a statement using casual WoW-style slang and attitude. "
-                "Respond as a real WoW player would: sarcastic, humorous, faction-proud, or boastful.",
+                "Comment aloud in character with a statement using casual WoW-style slang. "
+                "Respond as a real WoW player would. ",
                 botName, botLevel, botClass, botRace, botGender, botRole, botFaction,
                 botAreaName, botZoneName, botMapName,
                 personalityPrompt,
